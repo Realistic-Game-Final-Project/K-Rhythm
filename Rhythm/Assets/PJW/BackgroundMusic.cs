7cd0c1720da060593ca8b7f3c0a97bb1939aa094
pjw960316 <pjw960316@naver.com>
2021-11-24 22:18:21 +0900

    11/24 : bug(1) - 집에서 json 읽기 해보고 있는데 내가 유튜브에서 참고한 방식으로 테스트 json은 읽어지는데 inuyasha는 읽어지지 않음 - 주엽이가 성공했다고 하니 내일 팀플때 해보기

--- Rhythm/Assets/PJW/BackgroundMusic.cs
+++ Rhythm/Assets/PJW/BackgroundMusic.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ScalesInfo
 {
@@ -16,18 +17,38 @@ public class ScalesInfo
 
 public class BackgroundMusic : MonoBehaviour
 {
+
+    public TextAsset inuyasha_json;
+
+    [System.Serializable]
+    public class BackgroundMusicData
+    {
+        public int num;
+        public int duration;
+    }
+    [System.Serializable]
+    public class BackgroundMusicArray
+    {
+        public BackgroundMusicData[] background_music_data;
+    }
+
+    public BackgroundMusicArray background_music_array = new BackgroundMusicArray();
+    
     public const float SCALE_DURATION = 3f; //later , const -> value
-    public const int SCALES_COUNT = 3;
+    public const int SCALES_COUNT = 16;
     public const int AUDIO_SOURCE_COUNT = 4;
 
     public AudioClip[] original_clips = new AudioClip[SCALES_COUNT];
     private AudioSource[] mp3 = new AudioSource[AUDIO_SOURCE_COUNT];
+    private BackgroundMusicData background_music_data;
     private IEnumerator coroutine_obj;
 
     private void Awake()
     {
         Initialize();
-        StartCoroutine(PlayBackgroundMusic());      
+        StartCoroutine(PlayBackgroundMusic());
+        //GetJsonFromFile();
+        background_music_array = JsonUtility.FromJson<BackgroundMusicArray>(inuyasha_json.text);
     }
 
     private void Initialize()
@@ -43,10 +64,20 @@ public class BackgroundMusic : MonoBehaviour
         }        
     }
 
-    //TODO : 주엽이가 만든 데이터에서 음계번호를 읽어서 오디오 클립으로 변환 , 음악 지속 시간 추출
-    private void GetDataFromFirebaseAndConvertAudioclip()
+    //TODO : 주엽이가 만든 데이터에서 음계번호를 읽어서 오디오 클립으로 변환 , 음악 지속 시간 추출  
+    void GetJsonFromFile()
     {
-        
+        string json_path = Path.Combine(Application.dataPath, "PJW", "Inuyasha.json");
+        //string background_music_string = File.ReadAllText(json_path);  
+      
+        //background_music_data = JsonUtility.FromJson<BackgroundMusicData>(background_music_string);
+
+        ParseJson();     
+    }
+
+    private void ParseJson()
+    {
+
     }
 
     IEnumerator PlayBackgroundMusic()
