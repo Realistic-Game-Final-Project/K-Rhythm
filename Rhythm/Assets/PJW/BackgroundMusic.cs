ea4355b66df285c18f7691668c67e72196f192bf
pjw960316 <pjw960316@naver.com>
2021-11-24 13:38:17 +0900

    11/24 : feat(2)
    1. list + coroutine
    - 내 담당 리스트 형성
    - 해당 리스트로 음계 읽으면 자동으로 돌 코루틴 틀 구현
    - 아직 음악 파일 없어서 실제 구현 불가 (내일)
    
    2. git ignore

---
+++ Rhythm/Assets/PJW/BackgroundMusic.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalesInfo
+{
+    private int num;
+    private AudioClip clip;
+
+    ScalesInfo(int num , AudioClip clip)
+    {
+        this.num = num;
+        this.clip = clip;
+    }
+}
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public const float SCALE_DURATION = 3f; //later , const -> value
+    public const int SCALES_COUNT = 3;
+    public const int AUDIO_SOURCE_COUNT = 4;
+
+    public AudioClip[] original_clips = new AudioClip[SCALES_COUNT];
+    private AudioSource[] mp3 = new AudioSource[AUDIO_SOURCE_COUNT];
+    private IEnumerator coroutine_obj;
+
+    private void Awake()
+    {
+        Initialize();
+        StartCoroutine(PlayBackgroundMusic());      
+    }
+
+    private void Initialize()
+    {       
+        ScalesInfo[] scales_info = new ScalesInfo[SCALES_COUNT];
+        for(int i=0; i<AUDIO_SOURCE_COUNT; i++)
+        {
+            mp3 = gameObject.GetComponents<AudioSource>();
+        }
+        for(int i=0; i<SCALES_COUNT; i++)
+        {
+            //scales_info[i] = new ScalesInfo(i+1 , original_clips[i]);
+        }        
+    }
+
+    //TODO : 주엽이가 만든 데이터에서 음계번호를 읽어서 오디오 클립으로 변환 , 음악 지속 시간 추출
+    private void GetDataFromFirebaseAndConvertAudioclip()
+    {
+        
+    }
+
+    IEnumerator PlayBackgroundMusic()
+    {
+        //GetDataFromFirebaseAndConvertAudioclip()
+
+        coroutine_obj = PlayBackgroundMusic();
+        Debug.Log("coroutine Success");
+        /*
+         * 
+         * 
+         * 여기에 음계 별로 소리 재생하도록 코드 작성
+         * - 다양한 AudioSource 
+         * - 코루틴에서 변수(duration)를 받고 그 변수 만큼 음악 지속 하도록 구현
+         */
+
+        yield return new WaitForSeconds(SCALE_DURATION);
+
+        StopCoroutine(coroutine_obj);
+        coroutine_obj = null; //go GC?
+        StartCoroutine(PlayBackgroundMusic());
+    }
+
+}
