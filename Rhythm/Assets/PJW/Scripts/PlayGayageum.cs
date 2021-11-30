73de166d41f9188cee2c6274154976eb49bf1c99
pjw960316 <pjw960316@naver.com>
2021-11-30 17:24:21 +0900

    11/30 : 학교 작업

---
+++ Rhythm/Assets/PJW/Scripts/PlayGayageum.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//similar scripts , different consts and objects
+public class PlayGayageum : MonoBehaviour
+{
+    public const int SCALES_COUNT = 12; 
+
+    private Transform hitting_point_parent;
+    private GameObject[] hitting_points = new GameObject[SCALES_COUNT];
+    public AudioClip[] clips = new AudioClip[SCALES_COUNT];
+    private Dictionary<GameObject, AudioClip> music_dictionary = new Dictionary<GameObject, AudioClip>();
+    private AudioSource mp3;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        hitting_point_parent = GameObject.FindGameObjectWithTag("HittingPointGayageum").transform;
+        //Debug.Log(hitting_point_parent.name);
+        mp3 = gameObject.GetComponent<AudioSource>();
+        for (int i = 0; i < SCALES_COUNT; i++) //using index , never change
+        {
+            hitting_points[i] = hitting_point_parent.GetChild(i).gameObject;
+            music_dictionary.Add(hitting_points[i], clips[i]);
+        }
+    }
+
+    public void PlayInstrument(GameObject scale_object)
+    {
+        /*foreach(var a in music_dictionary)
+        {
+            Debug.Log(a.Key + " " + a.Value);
+        }*/
+       
+        mp3.clip = music_dictionary[scale_object];
+        mp3.Play(); //don't have to set end-time.
+    }
+}
