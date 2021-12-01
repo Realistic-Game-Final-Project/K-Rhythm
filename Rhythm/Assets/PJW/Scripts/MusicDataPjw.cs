daf7586cf9e619d964ca59acead4b8bd95bfb271
pjw960316 <pjw960316@naver.com>
2021-12-01 17:11:59 +0900

    12/01 : 코드 분할을 고민하고 기획 관련된 고민을 했음 - 뭔가를 정해야 정리가 될 것 같음 - 간단하게 미래를 위한 스크립트 생성

---
+++ Rhythm/Assets/PJW/Scripts/MusicDataPjw.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+//모든 스크립트에서 사용할 데이터 저장
+enum MUSIC_NUMBER
+{
+    INUYASHA = 1,
+    LETITGO = 2,
+    TMP = 3
+}
+
+//Every-Script Can Use Music Data 
+public class MusicDataPjw : MonoBehaviour
+{
+    //now just using music_inuyasha
+    public static List<Tuple<int, float>> music_inuyasha = new List<Tuple<int, float>>();
+    public static List<Tuple<int, float>> music_letitgo = new List<Tuple<int, float>>();
+    public static List<Tuple<int, float>> music_tmp = new List<Tuple<int, float>>();
+}
