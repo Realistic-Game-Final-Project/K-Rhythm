8422dd1bb63a6d2daa51c48079481bede71501a2
pjw960316 <pjw960316@naver.com>
2021-12-07 17:06:34 +0900

    12/07 : 그래픽 디자이너 에러 해결 및 내가 작업한 코드 (오후5시)

--- Rhythm/Assets/PJW/Scripts/StaticDataPjw.cs
+++ Rhythm/Assets/PJW/Scripts/StaticDataPjw.cs
@@ -5,15 +5,15 @@ using UnityEngine.SceneManagement;
 
 enum GAYAGEUM_SCALE_NUMBER //가야금 음을 한국말로 모름 
 {
-    ONE = 3,
-    TWO = 4,
-    THREE = 5,
-    FOUR = 6,
-    FIVE = 7,
-    SIX = 8,
-    SEVEN = 9,
-    EIGHT = 10,
-    NINE = 11
+    FOUR = 3,
+    FIVE = 4,
+    SIX = 5,
+    SEVEN = 6,
+    EIGHT = 7,
+    NINE = 8,
+    TEN = 9,
+    ELEVEN = 10,
+    TWELVE = 11
 }
 
 enum SCALE_ACCURACY_EASY
