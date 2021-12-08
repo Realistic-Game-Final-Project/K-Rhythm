0350af7ec99caaca43301b5279b8752e40765878
LeeJuYeob1 <80929433+LeeJuYeob1@users.noreply.github.com>
2021-12-08 17:33:26 +0900

    VRHand

---
+++ Rhythm/Assets/Lee/Scripts/IntruControl_Lee.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntruControl_Lee : MonoBehaviour
+{
+    public const string banghyang_scale_tag = "BangHyangScale";
+    public const string janggu_scale_tag = "JangguScale";
+    public const string gayageum_scale_tag = "GayageumScale";
+
+    public PlayBangHyang play_banghyang_script;
+    public PlayJangGu play_janggu_script;
+    public PlayGayageum play_gayageum_script;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        ExamineInstrumentType(collision.gameObject.tag, collision.gameObject);
+    }
+
+    private void ExamineInstrumentType(string tag, GameObject scale_object)
+    {
+        if (tag == banghyang_scale_tag)
+        {
+            Debug.Log(tag + "  " + scale_object.name);
+            play_banghyang_script.PlayInstrument(scale_object);
+        }
+        else if (tag == janggu_scale_tag)
+        {
+            /*
+             * TODO : janggu
+             */
+        }
+        else if (tag == gayageum_scale_tag)
+        {
+            Debug.Log(tag + "  " + scale_object.name);
+            play_gayageum_script.PlayInstrument(scale_object);
+        }
+    }
+
+}
