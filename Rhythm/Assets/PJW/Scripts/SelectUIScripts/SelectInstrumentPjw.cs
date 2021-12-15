6f55ef96bafe188bd634a6dda3ef527b9f257adc
pjw960316 <pjw960316@naver.com>
2021-12-15 23:34:18 +0900

    12/15 : feat(2) - 음악게임 이펙트 - Score , Music_Sheets 이미지 수정

--- Rhythm/Assets/PJW/Scripts/SelectUIScripts/SelectInstrumentPjw.cs
+++ Rhythm/Assets/PJW/Scripts/SelectUIScripts/SelectInstrumentPjw.cs
@@ -76,8 +76,10 @@ public class SelectInstrumentPjw : MonoBehaviour
         Invoke("Delay", DELAY_TIME);
     }
 
+    //Test
     private void Delay()
-    {       
-        SceneManager.LoadScene("PlayGameScene");
+    {
+        //SceneManager.LoadScene("PlayGameScene");   
+        SceneManager.LoadScene("Test2");
     }
 }
